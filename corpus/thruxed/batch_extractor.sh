5dc7b54061740cd919d0da2fd439ba8447a1a9db
shanahdt <daniel.shanahan@gmail.com>
2025-06-27 12:48:02 -0500

    re-extracted files

---
+++ corpus/thruxed/batch_extractor.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+### Gmajor key extraction for thruxed corpus
+for file in *.krn         
+do
+./key_extractor.pl $file -k "G:" > ../extracts/g-major/`basename $file`
+done
+
+
+### G-Freygish key extraction for thruxed corpus
+
+for file in *.krn         
+do
+./key_extractor.pl $file -k "G:frey" > ../extracts/g-freygish/`basename $file`
+done
+
+### G-minor key extraction for thruxed corpus
+
+for file in *.krn         
+do
+./key_extractor.pl $file -k "g:" > ../extracts/g-minor/`basename $file`
+done
+
+
+### G-dorian key extraction for thruxed corpus
+
+
+for file in *.krn         
+do
+./key_extractor.pl $file -k "g:dor" > ../extracts/g-raised-fourth/`basename $file`
+done
